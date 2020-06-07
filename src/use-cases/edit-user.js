47f7ce3fad48e8129185a34ff8d9deece9781196
joseph <12-10424@usb.ve>
2020-06-07 19:14:36 +0100

    added tern-port to gitignore

---
+++ src/use-cases/edit-user.js
@@ -0,0 +1,23 @@
+import makeUser from '../entities';
+
+export default function makeEditUser({ userDatabase }) {
+    return async function editUser({ id, ...changes } = {}) {
+        if (!id) {
+            throw new Error('You must supply an id.');
+        }
+
+        const existing = await userDatabase.findById({ id });
+
+        if (!existing) {
+            throw new RangeError('User not found');
+        }
+        const user = makeUser({ ...existing, ...changes });
+        const updated = await userDatabase.update({
+            id: user.getId(),
+            userName: user.getUsername(),
+            email: user.getEmail
+
+        });
+        return { ...existing, ...updated };
+    };
+}
